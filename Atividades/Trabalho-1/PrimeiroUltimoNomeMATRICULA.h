57c755336d609bb18c4c4f69b973f7a9dc41a1c0
George Neres <george.neres100@gmail.com>
2021-04-26 16:12:41 -0300

    Trabalho1

---
+++ Atividades/Trabalho-1/PrimeiroUltimoNomeMATRICULA.h
@@ -0,0 +1,23 @@
+
+/* Orienta��es Gerais --------
+Descri��o: esse arquivo deve conter as quest�es do trabalho do aluno.
+Cada aluno deve renomear esse arquivo para Aluno<MATRICULA>.c
+O aluno deve preencher seus dados abaixo, e implementar as questoes do trabalho.
+
+---------- Dados do Aluno --------------
+Nome:
+email:
+Matricula
+Semestre
+
+###################################
+EX:*/
+
+int somar(int x, int y); //função utilizada para testes
+int fatorial(int x); //função utilizada para testes
+int q1(char *data);
+int q2(char *datainicial, char *datafinal, int *qtdDias, int *qtdMeses, int *qtdAnos);
+int q3(char *texto, char c, int isCaseSensitive);
+int q4(char *strTexto, char *strBusca, int posicoes[30]);
+int q5(int num);
+int q6(int numerobase, int numerobusca);
